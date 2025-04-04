1512418e0c5723204428c52b0367a1187893b72d
kiaerii <kakabk35@gmail.com>
2025-04-04 23:35:53 +0300

    Update задание 4.py

--- lab_2/задание 4.py
+++ lab_2/задание 4.py
@@ -1,15 +1,8 @@
-# Импортируем модуль для работы с регулярными выражениями
 import re
-
-
-# Читаем файл text.txt
 data = open('text.txt').read()
-# Находим email адреса
-mails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b", data)
-# Находим номера телефонов
-phones = re.findall(r"\+7-\w{3}-\w{3}-\w{2}-\w{2}", data)
-# Находим слова начинающиеся с заглавной буквы
-capital = re.findall(r"\b[A-ZА-Я]\w*", data)
+mails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b", data) # Находим email адреса
+phones = re.findall(r"\+7-\w{3}-\w{3}-\w{2}-\w{2}", data) # Находим номера телефонов
+capital = re.findall(r"\b[A-ZА-Я]\w*", data) # Находим слова начинающиеся с заглавной буквы
 # Записываем результаты в файлы
 with open('emails.txt', 'w') as f:
 	for i in mails:
